1a69c8d9fd086b428c8740d91d147e137fcccf45
kyoutae1234 <73815569+kyoutae1234@users.noreply.github.com>
2024-12-23 20:45:34 +0900

    text popup
    텍스트 추가

---
+++ QuizFinder/Assets/Script/TriggerQuiz.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerQuiz : MonoBehaviour
+{
+    public string displayText; // 표시할 텍스트
+    public DynamicTextData textData; // 사용할 텍스트 데이터
+    public Vector3 textOffset = new Vector3(0, 2, 0); // 텍스트가 나타날 위치 오프셋
+    bool check = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // 플레이어가 충돌했는지 확인
+        if (other.CompareTag("Player") && !check)
+        {
+            // 텍스트를 생성
+            Vector3 spawnPosition = transform.position + textOffset;
+            //DynamicTextManager.Instance.Spawn3DText(spawnPosition, displayText, textData);
+
+            DynamicTextManager.CreateText(spawnPosition, displayText, textData);
+            check = true;
+        }
+    }
+}
