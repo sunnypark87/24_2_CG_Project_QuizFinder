c0c37b9ed2f3b13f02ac4ca75351a81969739a30
kyoutae1234 <73815569+kyoutae1234@users.noreply.github.com>
2024-12-23 21:33:45 +0900

    Pad added
    O, X 발판 추가됨

---
+++ QuizFinder/Assets/Script/Answer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Answer : MonoBehaviour
+{
+    public string displayText; // 표시할 텍스트 (O 또는 X)
+    public DynamicTextData textData; // 사용할 텍스트 데이터
+    public Vector3 textOffset = new Vector3(0, 2, 0); // 텍스트 위치 오프셋
+
+    // 활성화될 때 호출
+    private void OnEnable()
+    {
+        ShowText();
+    }
+
+    // 텍스트를 표시하는 공통 메서드
+    protected virtual void ShowText()
+    {
+        if (textData != null && !string.IsNullOrEmpty(displayText))
+        {
+            // 텍스트 표시
+            DynamicTextManager.CreateText(transform.position + textOffset, displayText, textData);
+        }
+    }
+}
--- QuizFinder/Assets/Script/TriggerQuiz.cs
+++ QuizFinder/Assets/Script/TriggerQuiz.cs
@@ -8,6 +8,7 @@ public class TriggerQuiz : MonoBehaviour
     public DynamicTextData textData; // 사용할 텍스트 데이터
     public Vector3 textOffset = new Vector3(0, 2, 0); // 텍스트가 나타날 위치 오프셋
     bool check = false;
+    public List<GameObject> answerObjects;
 
     private void OnTriggerEnter(Collider other)
     {
@@ -16,10 +17,23 @@ public class TriggerQuiz : MonoBehaviour
         {
             // 텍스트를 생성
             Vector3 spawnPosition = transform.position + textOffset;
-            //DynamicTextManager.Instance.Spawn3DText(spawnPosition, displayText, textData);
 
             DynamicTextManager.CreateText(spawnPosition, displayText, textData);
+
+            // Answer 태그를 가진 모든 오브젝트 활성화
+            ActivateAnswerObjects();
+
             check = true;
+        }
+    }
+
+    private void ActivateAnswerObjects()
+    {
+        foreach (GameObject answerObject in answerObjects)
+        {
+            Debug.Log(answerObject.name);
+            // 오브젝트 활성화
+            answerObject.SetActive(true);
         }
     }
 }
