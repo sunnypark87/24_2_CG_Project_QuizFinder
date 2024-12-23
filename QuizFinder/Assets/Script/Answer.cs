9c4b22e8c41291c2d81ff1e80f462e9e8c3591b7
kyoutae1234 <73815569+kyoutae1234@users.noreply.github.com>
2024-12-23 23:12:58 +0900

    correct
    정답 표시 추가

--- QuizFinder/Assets/Script/Answer.cs
+++ QuizFinder/Assets/Script/Answer.cs
@@ -11,13 +11,18 @@ public class Answer : MonoBehaviour
     public string displayText; // 표시할 텍스트 (O 또는 X)
     public DynamicTextData textData; // 사용할 텍스트 데이터
     public Vector3 textOffset = new Vector3(0, 2, 0); // 텍스트 위치 오프셋
-
-    public bool quizAnswer;
+    public TriggerQuiz triggerQuiz;
+    protected bool thisAnswer;
 
     // 활성화될 때 호출
     private void OnEnable()
     {
         ShowText();
+
+        if (triggerQuiz == null)
+        {
+            Debug.LogError("QuizTrigger를 찾을 수 없습니다.");
+        }
     }
 
     // 텍스트를 표시하는 공통 메서드
@@ -27,6 +32,15 @@ public class Answer : MonoBehaviour
         {
             // 텍스트 표시
             DynamicTextManager.CreateText(transform.position + textOffset, displayText, textData);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // 플레이어가 충돌했는지 확인
+        if (other.CompareTag("Player"))
+        {
+            triggerQuiz.SubmitAnswer(thisAnswer);
         }
     }
 }
